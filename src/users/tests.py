e1cb1b2bcfd114b215f3adbf6f9e6686b4e3bfd4
Snape <serg.serg2006@mail.ru>
2025-07-10 22:47:29 +0300

    Create tests for user

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserTests(APITestCase):
+    def test_create_user(self):
+        url = reverse("users:sign-up")
+        data = {"username": "user1", "password": "password123"}
+        self.client.post(url, data)
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.get(username=data["username"])
+        self.assertEqual(user.username, data["username"])
